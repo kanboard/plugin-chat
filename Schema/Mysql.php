9813f576645c9c3842629bbf619b3707620b9782
Frederic Guillot <fred@kanboard.net>
2017-03-05 11:14:27 -0500

    Fix typo in Mysql schema

--- Schema/Mysql.php
+++ Schema/Mysql.php
@@ -13,12 +13,13 @@ function version_1(PDO $pdo)
         `message` TEXT NOT NULL,
         `user_id` INT NOT NULL,
         `creation_date` INT NOT NULL,
-        FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+        FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+        PRIMARY KEY(id)
     ) ENGINE=InnoDB CHARSET=utf8');
 
     $pdo->exec('CREATE TABLE chat_sequences (
-        "user_id" INT "users" NOT NULL UNIQUE,
-        "message_id" INT NOT NULL,
+        `user_id` INT NOT NULL UNIQUE,
+        `message_id` INT NOT NULL,
         FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
     ) ENGINE=InnoDB CHARSET=utf8');
 }
