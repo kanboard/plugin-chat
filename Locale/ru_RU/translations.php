7ba02152bada90e9a8b67f823685b0a240cb86c8
Dmitry <kochevd@gmail.com>
2017-03-07 01:16:05 +0300

    RU locale

---
+++ Locale/ru_RU/translations.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'There is no message.' => 'Сообщений нет.',
+    'Refresh Interval' => 'Интервал обновления',
+    'Period in second (3 seconds by default)' => 'Период в секундах (по умолчанию - 3 секунды)',
+    'Chat' => 'Чат',
+    'Minimalist Chat for Kanboard.' => 'Минималистичный чат для Kanboard.',
+);
