276f44b70b8025968b816b6befb8667e3900538a
Timo <tili2@gmx.de>
2017-03-06 11:01:41 +0100

    de_DE translation

---
+++ Locale/de_DE/translations.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'There is no message.' => 'Es gibt keine Nachrichten.',
+    'Refresh Interval' => 'Aktualisierungsintervall',
+    'Period in second (3 seconds by default)' => 'Zeitraum in Sekunden (3 Sekunden standardmäßig)',
+    'Chat' => 'Chat',
+    'Minimalist Chat for Kanboard.' => 'Minimalistischer Chat für Kanboard.',
+);
