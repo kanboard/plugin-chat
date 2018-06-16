9822a0ededd665077e632f0e9898cf12a01da883
Balázs Úr <urbalazs@gmail.com>
2018-06-16 17:33:43 +0200

    Updated Hungarian translation

---
+++ Locale/hu_HU/translations.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'There is no message.' => 'Nincs üzenet.',
+    'Refresh Interval' => 'Frissítési időköz',
+    'Period in second (3 seconds by default)' => 'Időszak másodpercben (alapértelmezetten 3 másodperc)',
+    'Chat' => 'Csevegés',
+    'Minimalist Chat for Kanboard.' => 'Minimalista csevegés a Kanboard programhoz.',
+);
